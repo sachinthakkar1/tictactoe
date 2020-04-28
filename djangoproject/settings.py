a48723ba00b68a843ef0d69b81cc1e79b383be55
sachin <17ceuog044@gmail.com>
2020-04-28 18:56:37 +0530

    changed settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -76,15 +76,6 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sample',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '4040',
-    }
-    
 }
 
 
